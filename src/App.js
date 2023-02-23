bd6822b659b997858f12d582a4529a9bcb98bc98
Elena <olena168@gmail.com>
2023-02-23 22:36:45 +0300

    finished app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import animal from './animal.jpg';
+
 
 function App() {
+  const [answer, setAnswer] = useState('');
+  const [advice, setAdvice] = useState('');
+  const [btn, setBtn] = useState('FIND ACTIVITY');
+
+  useEffect(() => {
+    const getAnswer = async () => {
+      const response = await fetch ('http://www.boredapi.com/api/activity/');
+      const data = await response.json();
+      setAnswer (data.activity);
+    }
+    getAnswer();  
+  }, [advice])
+
+  const getNewAnswer = () => {
+    setAnswer (setAdvice);
+    if(btn === 'FIND ACTIVITY') {
+      setBtn ('TRY AGAIN')
+    }
+    else {
+      setBtn ('FIND ACTIVITY')
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div>
+        <img className='page_back' src={animal} alt='pic'/>
+      </div>
+      <div className='container'>
+        <h1>What would you like to do when you are bored?</h1>
+        <p>{answer}</p>
+        <p></p>
+      </div>
+      <div className='btn'>  
+        <button onClick={getNewAnswer}>{btn}</button>
+      </div>
     </div>
   );
 }
