3bdc67f39ef04d5c178e9e9c02b5109419d0652b
Elena <olena168@gmail.com>
2024-08-19 22:25:30 -0400

    change api

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import animal from './animal.jpg';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import photo from "./animal.jpg";
 
 function App() {
-  const [answer, setAnswer] = useState('');
-  const [advice, setAdvice] = useState('');
-  const [btn, setBtn] = useState('FIND ACTIVITY');
+  const [answer, setAnswer] = useState("");
+  const [advice, setAdvice] = useState("");
+  const [btn, setBtn] = useState("FIND ACTIVITY");
 
   useEffect(() => {
     const getAnswer = async () => {
-      const response = await fetch ('http://www.boredapi.com/api/activity/');
+      const response = await fetch("https://bored-api.appbrewery.com/random");
       const data = await response.json();
-      setAnswer (data.activity);
-    }
-    getAnswer();  
-  }, [advice])
+      setAnswer(data.activity);
+      console.log(data);
+    };
+    getAnswer();
+  }, [advice]);
 
   const getNewAnswer = () => {
-    setAnswer (setAdvice);
-    if(btn === 'FIND ACTIVITY') {
-      setBtn ('TRY AGAIN')
+    setAnswer(setAdvice);
+    if (btn === "FIND ACTIVITY") {
+      setBtn("TRY AGAIN");
+    } else {
+      setBtn("FIND ACTIVITY");
     }
-    else {
-      setBtn ('FIND ACTIVITY')
-    }
-  }
-
+  };
 
   return (
-    <div className='container'>
+    <div className="container">
       <div>
-        <img className='page_back' src={animal} alt='pic'/>
+        <img className="page_back" src={photo} alt="pic" />
       </div>
-      <div className='container'>
+      <div className="container">
         <h1>What would you like to do when you are bored?</h1>
         <p>{answer}</p>
         <p></p>
       </div>
-      <div className='btn'>  
+      <div className="btn">
         <button onClick={getNewAnswer}>{btn}</button>
       </div>
     </div>
